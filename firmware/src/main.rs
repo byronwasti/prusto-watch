cad4335ad8db4301aba3281bf65758e840a21301
Byron Wasti <byron.wasti@gmail.com>
2017-12-14 14:05:21 -0500

    Add RTFM based firmware for interrupt driven blinky

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,37 +3,43 @@
 
 extern crate cortex_m_rtfm as rtfm;
 extern crate stm32f30x;
+extern crate cortex_m_semihosting;
 
 use core::u16;
 use rtfm::{app, Threshold};
+
+use core::fmt::Write;
+use cortex_m_semihosting::hio;
 
 app! {
     device: stm32f30x,
 
     resources: {
         static ON: bool = false;
+        static COUNTER: u32 = 0;
     },
 
     tasks: {
         TIM7: {
             path: toggle,
-            resources: [ON, GPIOE],
+            resources: [ON, COUNTER, GPIOA],
         },
     },
 }
 
 fn init(p: init::Peripherals, _r: init::Resources) {
-    let gpioe = p.GPIOE;
+    let gpioa = p.GPIOA;
     let rcc = p.RCC;
     let tim7 = p.TIM7;
 
-    rcc.ahbenr.modify(|_, w| w.iopeen().enabled());
+    rcc.ahbenr.modify(|_, w| w.iopaen().enabled());
     rcc.apb1enr.modify(|_, w| w.tim7en().enabled());
 
-    gpioe.moder.modify(|_, w| w.moder9().output());
+    gpioa.moder.modify(|_, w| w.moder9().output());
 
     let ratio = 8_000_000;
-    let psc = ((ratio - 1) /(u16::MAX as u32)) as u16;
+    //let psc = ((ratio - 1) /(u16::MAX as u32)) as u16;
+    let psc = u16::MAX - 100;
     tim7.psc.write(|w| w.psc().bits(psc));
     let arr = (ratio / ((psc + 1) as u32)) as u16;
     tim7.arr.write(|w| w.arr().bits(arr));
@@ -51,14 +57,18 @@ fn idle() -> ! {
 }
 
 fn toggle(_t: &mut Threshold, r: TIM7::Resources) {
-    // Toggle our state
-    **r.ON = !**r.ON;
-
-    let gpioe = &**r.GPIOE;
-    if **r.ON {
-        gpioe.bsrr.write(|w| w.bs9().set());
+    if (**r.COUNTER > 10000) {
+        // Toggle our state
+        **r.ON = !**r.ON;
+        **r.COUNTER = 0;
+        let gpioa = &**r.GPIOA;
+        if **r.ON {
+            gpioa.bsrr.write(|w| w.bs9().set());
+        } else {
+            gpioa.bsrr.write(|w| w.br9().reset());
+        }
     } else {
-        gpioe.bsrr.write(|w| w.br9().reset());
+        **r.COUNTER += 1;
     }
 }
 
